42c9e9c627f7c7b04eae94776ba78479a110f544
Jennn <jenny091101@gmail.com>
2023-04-22 04:26:51 +0800

    att: 整理function，並更改convert function

--- att.py
+++ att.py
@@ -14,26 +14,46 @@ import matplotlib.pyplot as plt
 import linecache
 
 training_dir = "./faces/training/"
-test_dir = "./faces/testing/"
-txt_root = "./attPath.txt" # 放所有att檔案的path
+testing_dir = "./faces/testing/"
+training_txt_root = "./attPath-Train.txt" # 放所有att檔案的path
+testing_txt_root  = "./attPath-Test.txt" # 放所有att檔案的path
 train_batch_size = 10
+train_number_epochs = 10
+MODELS_PATH = './models'
+LR = 0.0005
+Gaussian_factor = 15
 
 # 把所有att檔案的path 寫到同個txt裡
 def convert(train=True):
     if(train):
-        f=open(txt_root, 'w')
-        data_path=training_dir
-        if(not os.path.exists(data_path)):
-            os.makedirs(data_path)
-        for i in range(40):
-              for j in range(10):
-                    img_path = data_path+'s'+str(i+1)+'/'+str(j+1)+'.pgm'
-                    f.write(img_path+' '+str(i)+'\n')      
-        f.close()
+        txt_root = training_txt_root
+        data_path = training_dir
+    else:
+        txt_root = testing_txt_root
+        data_path = testing_dir
+
+    if(not os.path.exists(data_path)):
+        os.makedirs(data_path)
+    
+    f=open(txt_root, 'w')
+    # 參考 https://newaurora.pixnet.net/blog/post/228299675-python%E8%AE%80%E5%8F%96%E8%B3%87%E6%96%99%E5%A4%BE%E5%85%A7%E6%AA%94%E6%A1%88
+    # walk 會將指定路徑底下所有的目錄與檔案都列出來(包含子目錄以及子目錄底下的檔案)
+    allList = os.walk(data_path)
+
+    num = 0
+    # 列出所有子目錄與子目錄底下所有的檔案
+    for root, dirs, files in allList:
+        # dir = root.split('/')[-1][1:]
+        for i in files:
+            if(i[0] == 'R'): continue # README不要讀進去
+            img_path = root + '/' + i
+            f.write(img_path + ' ' + str(num) +'\n')  
+            num += 1
+    f.close()
 
 # 高斯模糊
 class AddGaussianNoise(object):
-    def __init__(self, mean=0., std=45.):
+    def __init__(self, mean=0., std=Gaussian_factor):
         self.std = std
         self.mean = mean
         
@@ -51,17 +71,17 @@ class SiameseNetworkDataset(Dataset):
         self.txt = txt       # 之前生成的train.txt
 
     def __getitem__(self, index):
-        line = linecache.getline(self.txt, random.randint(1, self.__len__()))   # 随机选择一个人脸
+        line = linecache.getline(self.txt, random.randint(1, self.__len__()))   # 隨機選擇一個人臉
         line.strip('\n')
         img0_list= line.split()
-        should_get_same_class = random.randint(0,1)     # 随机数0或1，是否选择同一个人的脸，这里为了保证尽量使匹配和非匹配数据大致平衡（正负类样本相当）
-        if should_get_same_class:    # 执行的话就挑一张同一个人的脸作为匹配样本对
-                while True:
-                    img1_list = linecache.getline(self.txt, random.randint(1, self.__len__())).strip('\n').split()
-                    if img0_list[1]==img1_list[1]:
-                            break
-        else:       # else就是随意挑一个人的脸作为非匹配样本对，当然也可能抽到同一个人的脸，概率较小而已
-                img1_list = linecache.getline(self.txt, random.randint(1,self.__len__())).strip('\n').split()
+        should_get_same_class = random.randint(0,1)     # 隨機數0或1，是否選擇同一人的臉，這裡為了導正盡量使匹配和非匹配數據大致平衡 (正負類樣本相當) 
+        if should_get_same_class:   # 執行的話就挑一張同一個人的臉作為匹配樣本對
+            while True:
+                img1_list = linecache.getline(self.txt, random.randint(1, self.__len__())).strip('\n').split()
+                if img0_list[1]==img1_list[1]:
+                    break
+        else:       # else就是随意挑一個人的臉作為非匹配樣本對，當然也可能抽到同一個人的臉，概率較小而已
+            img1_list = linecache.getline(self.txt, random.randint(1,self.__len__())).strip('\n').split()
         
         img0 = Image.open(img0_list[0])    # img_list都是大小为2的列表，list[0]为图像, list[1]为label
         img1 = Image.open(img1_list[0])
@@ -76,7 +96,7 @@ class SiameseNetworkDataset(Dataset):
             img0 = self.transform(img0)
             img1 = self.transform(img1)
     
-        return img0, img1 , torch.from_numpy(np.array([int(img1_list[1]!=img0_list[1])],dtype=np.float32))    # 注意一定要返回数据+标签， 这里返回一对图像+label（应由numpy转为tensor）
+        return img0, img1 , torch.from_numpy(np.array([int(img1_list[1]!=img0_list[1])], dtype=np.float32))    # 注意一定要返回数据+标签， 这里返回一对图像+label（应由numpy转为tensor）
     
     def __len__(self):       # 数据总长
         fh = open(self.txt, 'r')
@@ -105,7 +125,7 @@ class SiameseNetwork(nn.Module):
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(8),
             nn.Dropout2d(p=.2),
-            )
+        )
         
         self.fc1 = nn.Sequential(
             nn.Linear(8*100*100, 500),
@@ -115,7 +135,7 @@ class SiameseNetwork(nn.Module):
             nn.ReLU(inplace=True),
             
             nn.Linear(500, 5)
-            )
+        )
 
     def forward_once(self, x):
         output = self.cnn1(x)
@@ -140,32 +160,55 @@ class ContrastiveLoss(torch.nn.Module):
 
         return loss_contrastive
 
+
 if __name__ == '__main__':
-    train_data = SiameseNetworkDataset(txt = txt_root,
-                                       transform=transforms.Compose([
-                                           transforms.Resize((100,100)),
-                                           transforms.ToTensor(),
-                                           AddGaussianNoise(0., 45.)
-                                           ]), 
+    if input("write txt? [press enter with NO rewrite]: "): # 寫檔
+        convert(train=True)
+        convert(train=False)
+    
+    print("Total epoch:", train_number_epochs,",  Batch size:",train_batch_size, ",  LR:",LR, ",  Gaussian Factor:", Gaussian_factor)
+    test_transform=transforms.Compose([
+        transforms.Resize((100,100)),
+        transforms.ToTensor(),
+        # torchvision.transforms.Normalize((.5, .5, .5), (.5, .5, .5)),
+        AddGaussianNoise(0., 45.)
+    ])
+    
+    train_data = SiameseNetworkDataset(txt = training_txt_root,
+                                       # train=True,
+                                       transform=test_transform, 
                                        should_invert=False)     #Resize到100,100
-    train_dataloader = DataLoader(dataset=train_data, shuffle=True, num_workers=2, batch_size = train_batch_size)
+    
+    test_data = SiameseNetworkDataset(txt = testing_txt_root,
+                                       # train=False,
+                                       transform=test_transform, 
+                                       should_invert=False)     #Resize到100,100
+    
+    
+    train_dataloader = DataLoader(
+        dataset=train_data, 
+        shuffle=True, 
+        num_workers=2, 
+        batch_size = train_batch_size
+    )
 
     #net = SiameseNetwork().cuda()     # GPU加速
     net = SiameseNetwork()
     criterion = ContrastiveLoss()
-    optimizer = optim.Adam(net.parameters(), lr=0.0005)
+    optimizer = optim.Adam(net.parameters(), lr=LR)
 
 
     counter = []
     loss_history =[]
     iteration_number =0
-    train_number_epochs = 10
 
+    net.train()
     for epoch in range(1, train_number_epochs + 1):
         for i, data in enumerate(train_dataloader, 0):
             img0, img1, label = data
             img0, img1, label = Variable(img0), Variable(img1), Variable(label)
             output1, output2 = net(img0, img1)
+
             optimizer.zero_grad()
             loss_contrastive = criterion(output1, output2, label)
             loss_contrastive.backward()
@@ -177,5 +220,27 @@ if __name__ == '__main__':
                 counter.append(iteration_number)
                 loss_history.append(loss_contrastive.item())
     
+    if not os.path.exists(MODELS_PATH):
+        os.mkdir(MODELS_PATH)
+    torch.save(net, os.path.join(MODELS_PATH, 'att_model.pt'))
+    
+    """
+    test_loader = DataLoader(
+        dataset=test_data, 
+        shuffle=False, 
+        num_workers=2, 
+        batch_size = len(test_data) # train_batch_size
+    )
+
+    test_x, test_y = next(iter(test_loader))
+
+    net.eval()
+    prediction = torch.argmax(net(test_x), 1)
+    acc = torch.eq(prediction, test_y)
+    print('Accuracy: {:.2%}'.format(
+            (torch.sum(acc) / acc.shape[0]).item())
+         )
+    """
+
     plt.plot(counter, loss_history)
-    plt.show()     # plot 损失函数变化曲线
+    plt.show()     # plot 損失函數變化曲線
--- cifar.py
+++ cifar.py
@@ -57,51 +57,51 @@ data_loader = Data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle
 classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
 class CNN(nn.Module):
-  def __init__(self, num_classes: int):
-    super(CNN, self).__init__()
-    self.num_classes = num_classes
+    def __init__(self, num_classes: int):
+        super(CNN, self).__init__()
+        self.num_classes = num_classes
 
-    # in[N, 3, 32, 32] => out[N, 16, 16, 16]
-    self.conv1 = nn.Sequential(
-        nn.Conv2d(
-            in_channels=3,
-            out_channels=16,
-            kernel_size=5,
-            stride=1,
-            padding=2
-        ),
-        nn.ReLU(True),
-        nn.MaxPool2d(kernel_size=2)
-    )
-    # in[N, 16, 16, 16] => out[N, 32, 8, 8]
-    self.conv2 = nn.Sequential(
-        nn.Conv2d(16, 32, 5, 1, 2),
-        nn.ReLU(True),
-        nn.MaxPool2d(2)
+        # in[N, 3, 32, 32] => out[N, 16, 16, 16]
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=3,
+                out_channels=16,
+                kernel_size=5,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        # in[N, 16, 16, 16] => out[N, 32, 8, 8]
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(16, 32, 5, 1, 2),
+            nn.ReLU(True),
+            nn.MaxPool2d(2)
 
-    )
-    # in[N, 32 * 8 * 8] => out[N, 128]
-    self.fc1 = nn.Sequential(
-        nn.Linear(32 * 8 * 8, 128),
-        nn.ReLU(True)
-    )
-    # in[N, 128] => out[N, 64]
-    self.fc2 = nn.Sequential(
-        nn.Linear(128, 64),
-        nn.ReLU(True)
-    )
-    # in[N, 64] => out[N, 10]
-    self.out = nn.Linear(64, self.num_classes)
+        )
+        # in[N, 32 * 8 * 8] => out[N, 128]
+        self.fc1 = nn.Sequential(
+            nn.Linear(32 * 8 * 8, 128),
+            nn.ReLU(True)
+        )
+        # in[N, 128] => out[N, 64]
+        self.fc2 = nn.Sequential(
+            nn.Linear(128, 64),
+            nn.ReLU(True)
+        )
+        # in[N, 64] => out[N, 10]
+        self.out = nn.Linear(64, self.num_classes)
 
-  def forward(self, x):
-    x = self.conv1(x)
-    x = self.conv2(x)
-    x = x.view(x.size(0), -1) # [N, 32 * 8 * 8]
-    x = self.fc1(x)
-    x = self.fc2(x)
-    output = self.out(x)
-    return output
-  
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.view(x.size(0), -1) # [N, 32 * 8 * 8]
+        x = self.fc1(x)
+        x = self.fc2(x)
+        output = self.out(x)
+        return output
+    
 cnn = CNN(len(classes))
 
 optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)
@@ -109,22 +109,22 @@ optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)
 loss_function = nn.CrossEntropyLoss()
 
 for epoch in range(EPOCH):
-  cnn.train()
-  for step, (x, y) in enumerate(data_loader):
-    b_x = Variable(x, requires_grad=False)
-    b_y = Variable(y, requires_grad=False)
-    out = cnn(b_x)
-    loss = loss_function(out, b_y)
+    cnn.train()
+    for step, (x, y) in enumerate(data_loader):
+        b_x = Variable(x, requires_grad=False)
+        b_y = Variable(y, requires_grad=False)
+        out = cnn(b_x)
+        loss = loss_function(out, b_y)
 
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-    if step % 100 == 0:
-        print('Epoch: {} | Step: {} | Loss: {}'.format(epoch + 1, step, loss))
+        if step % 100 == 0:
+            print('Epoch: {} | Step: {} | Loss: {}'.format(epoch + 1, step, loss))
 
 if not os.path.exists(MODELS_PATH):
-  os.mkdir(MODELS_PATH)
+    os.mkdir(MODELS_PATH)
 torch.save(cnn, os.path.join(MODELS_PATH, 'cnn_model.pt'))
 
 test_loader = Data.DataLoader(
